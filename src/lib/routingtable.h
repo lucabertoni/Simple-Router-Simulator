e8fadb0cf56d5f52b894682a1eceb222fdf38d1b
Luca Bertoni <luca.bertoni24@gmail.com>
2015-12-21 20:24:24 +0100

    Update source

---
+++ src/lib/routingtable.h
@@ -0,0 +1,18 @@
+#ifndef ROUTINGTABLE_H
+#define ROUTINGTABLE_H
+
+// Struct che definisce una singola riga della routing table
+struct routing_table_row
+{
+	int ip;			// Ip corrente
+	int next_hop;	// Passo successivo
+	int peso;		// Peso per raggiungere il nodo successivo
+};
+
+// Struct che definisce l'intera tabella di routing basandosi sulle singole righe della routing table
+struct routing_table
+{
+	struct routing_table_row* table_rows;
+};
+
+#endif
